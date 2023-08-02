6c455e82702d062367367aa765f0eb82d288eebd
Marc0Franc0 <marcofranco340@gmail.com>
2023-08-02 00:52:41 -0300

    Update SecurityConfig.java
    Se agregaron los filtros de autenticación y autorización creados en el contexto de la aplicación.

--- src/main/java/com/app/config/SecurityConfig.java
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -1,36 +1,47 @@
 package com.app.config;
 
+import com.app.jwt.JwtAuthenticationFilter;
+import com.app.jwt.JwtAuthorizationFilter;
+import com.app.jwt.JwtTokenProvider;
 import com.app.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Autowired
     UserDetailsServiceImpl userDetailsService;
-
+    @Autowired
+    JwtAuthorizationFilter jwtAuthorizationFilter;
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
     //Este objeto se encarga de la administración de la autenticación de los usuarios
     @Bean
     AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder)
             throws Exception {
-    return httpSecurity
-            .getSharedObject(AuthenticationManagerBuilder.class)
-            .userDetailsService(userDetailsService)
-            .passwordEncoder(passwordEncoder)
-            .and()
-            .build();
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
     }
+
 
     //Objeto el cual se encarga de la encriptación de contraseñas
     @Bean
@@ -45,17 +56,35 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager)
             throws Exception {
+        //Se utiliza el filtro de autenticación creado
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider);
+        //Se le establece un AutenticacionManager para que pueda funcionar correctamente el cual
+        //es creado dentro de esta clase
+        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        //Se configura el endpoint para autenticarse
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
         return http
+                // Se deshabilita Cross-site request forgery
                 .csrf(config->config.disable())
                 .sessionManagement(session->{
+                    // Permite la gestion de sesiones de tipo STATELESS
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 .authorizeHttpRequests(
-                        auth->
-                                auth.requestMatchers("/").permitAll()
+                        //Configuración de acceso a los endpoints
+                        auth-> {
+                            auth.anyRequest().permitAll();
+                        }
                 )
+                //Filtro creado el cual es necesario para autenticar un usuario con su username y password
+                .addFilter(jwtAuthenticationFilter)
+                //Filtro creado el cual es necesario para autorizar utilizando un token
+                //Se ejecuta luego del filtro de autenticación
+                .addFilterBefore(jwtAuthorizationFilter,UsernamePasswordAuthenticationFilter.class)
                 .build();
 
 
     }
+
 }
